15177fcedc8d1fb7b69f5df02259ab331576c93a
brunogalerani <bruno_galerani@hotmail.com>
2018-04-14 07:11:14 -0300

    Fixing the level1 map and imlementing the end level!

--- violent conflict/Assets/Scripts/LevelController.cs
+++ violent conflict/Assets/Scripts/LevelController.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LevelController : MonoBehaviour {
 
     public GameObject[] characters;
+    public GameObject[] enemyPrefabs;
+    public Transform[] enemySpawnPoints;
     public Transform spawnPoint;
+    public Transform winPoint;
+    public Canvas canvas;
 
     private GameState gameState;
     private GameObject currentCharacter;
@@ -14,10 +19,18 @@ public class LevelController : MonoBehaviour {
 	void Start () {
         gameState = GameState.Instance;
         currentCharacter = Instantiate(characters[gameState.GetCurrentSelectedCharacter()], spawnPoint.position, Quaternion.identity);
+        for(int i = 0; i < enemySpawnPoints.Length; i++)
+        {
+            int enemyIndex = Random.Range(0, enemyPrefabs.Length);
+            //Instantiate(enemyPrefabs[enemyIndex], enemySpawnPoints[i].position, Quaternion.identity);
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Vector3.Distance(currentCharacter.transform.position, winPoint.position) < 1f)
+        {
+            SceneManager.LoadSceneAsync("PlayerSelect");
+        }
 	}
 }
